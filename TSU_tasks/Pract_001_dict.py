559723bd661ca16861f019dee12977672fc54dde
evro <evvro30@yandex.ru>
2022-06-11 13:23:37 +0300

    change Pract_001_dict

--- TSU_tasks/Pract_001_dict.py
+++ TSU_tasks/Pract_001_dict.py
@@ -1,4 +1,4 @@
-# Практика №1
+# Практика №1 (Практическое_задание_1_ВрЕН) в Юпитер
 # 1) Сначала делаем словарь пользователей соц сети(11 участников) ----------------------------------------------------
 users = [
     {"id": 1, "name": "Алекс"},
@@ -22,8 +22,9 @@ print(type(users))
 fp = [(1, 4), (1, 8), (2, 5), (2, 6), (2, 11), (3, 5), (3, 6), (3, 11), (4, 1), (4, 5), (4, 11), (5, 2), (5, 3),
       (5, 4), (5, 7), (6, 2), (6, 3), (6, 9), (7, 5), (7, 8), (8, 1), (8, 7), (8, 10), (9, 6), (9, 10), (10, 8),
       (10, 9), (11, 2), (11, 3), (11, 4)]
-print(type(fp))
-print(fp)
+# print(type(fp))
+# print(fp)
+
 # !!ради интереса хотел вывести длинный список построчно с заданным кол-вом кортежей в строке
 # count = 0
 # a = 4
@@ -42,14 +43,27 @@ print(fp)
 
 # 3)Инициализировать словарь пустым списком для идентификатора каждого пользователя
 friendships = {user["id"]: [] for user in users}
-print(type(friendships), friendships)
+# print(type(friendships), friendships)
+
 for i, j in fp:
     friendships[i].append(j)  # Добавить j как друга для i
     friendships[j].append(i)  # Добавить i как друга ДЛЯ j
-print(friendships)
+# print(friendships)
 for key, value in friendships.items():
     friendships[key] = sorted(set(value))  # исключим обратные ссылки(задвоенность и для удобства отсортируем)
-print(friendships)
+print('dict', friendships)
 
 for k, v in friendships.items():
     print(k, v)
+
+# 4)Создадим функцию определяющяя количество друзей у выбранного пользователя
+print(users[10])
+def number_of_friends(u):
+    user_id = u['id']
+    friend_ids = friendships[user_id]
+    return len(friend_ids)
+
+
+print(users[10]['name'], number_of_friends(users[10]))
+
+# 5)Создадим функцию, выводящую на экран пользователей, отсортированных по количеству друзей
